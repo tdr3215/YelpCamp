1c796d49f0dca2da55916ede150305d163a1ae86
Tiffany Rose <tiffany.drose@gmail.com>
2021-09-08 22:14:23 -0400

    initial express app set up

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 app.listen("3000", () => {
-  console.log("LISTENING ON 3000");
+  console.log("LISTENING ON PORT 3000");
 });
