51eb8dc2d06fc532823718dd3037d0bc01afe7d7
Tiffany Rose <tiffany.drose@gmail.com>
2021-09-09 00:52:58 -0400

    added method override to incorporate PUT/DELETE methods and added UPDATE component

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
+const methodOverride = require("method-override");
 const path = require("path");
 const Campground = require("./models/campground");
 
@@ -22,24 +23,64 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-// EXPRESS REQUIREMENTS
+// ACCESS VIEWS DIR GLOBALLY
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.urlencoded({ extended: true }));
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true })); //PARSING req.body
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard",
-    description: "Cheap camping",
-  });
-  await camp.save();
-  res.send("CAMP");
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
+
+// HOME
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// NEW DOC
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// SHOW DOC
+app.get("/campgrounds/:id", async (req, res) => {
+  const campgrounds = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campgrounds });
+});
+
+// EDIT DOC
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campgrounds = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campgrounds });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  await Campground.findByIdAndUpdate(
+    id,
+    { ...req.body.campground },
+    { new: true }
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// DELETE DOC
+
 
 app.listen("3000", () => {
   console.log("LISTENING ON PORT 3000");
 });
+
+// 6139844832d0dbc38e91c456
+// 6139844832d0dbc38e91c452
