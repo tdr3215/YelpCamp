89f6b5338330614b34a0888298767dfdfe58ae2f
Tiffany Rose <tiffany.drose@gmail.com>
2021-09-25 18:43:38 -0400

    adding delete functionality

--- app.js
+++ app.js
@@ -152,6 +152,16 @@ app.post(
   })
 );
 
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 // ERROR HANDLING
 
 app.all("*", (req, res, next) => {
